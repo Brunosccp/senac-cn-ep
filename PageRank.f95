10a3e759475c83d4f29bbb5f4b20f2c3cf53bd2a
Brunosccp <rocca_bruno@hotmail.com>
2018-06-13 02:18:11 -0300

    adicionando base que calcula os pesos

--- PageRank.f95
+++ PageRank.f95
@@ -6,24 +6,26 @@ PROGRAM PageRank
     double precision, dimension(:), allocatable :: V
 
     A(1,1) = 0
-    A(2,1) = 0
-    A(3,1) = 1
-    A(4,1) = 1/2.0
+    A(1,2) = 0
+    A(1,3) = 1
+    A(1,4) = 1/2.0
 
-    A(1,2) = 1/3.0
+    A(2,1) = 1/3.0
     A(2,2) = 0
-    A(3,2) = 0
-    A(4,2) = 0
+    A(2,3) = 0
+    A(2,4) = 0
 
-    A(1,3) = 1/3.0
-    A(2,3) = 1/2.0
+    A(3,1) = 1/3.0
+    A(3,2) = 1/2.0
     A(3,3) = 0
-    A(4,3) = 1/2.0
+    A(3,4) = 1/2.0
 
-    A(1,4) = 1/3.0
-    A(2,4) = 1/2.0
-    A(3,4) = 0
+    A(4,1) = 1/3.0
+    A(4,2) = 1/2.0
+    A(4,3) = 0
     A(4,4) = 0
+
+    !print *, A
 
     V = calcPageRank(A)
 
@@ -32,32 +34,56 @@ PROGRAM PageRank
     contains
     function calcPageRank(A)
         double precision, dimension(:, :) :: A
-        real :: x
-        double precision :: temp
-        integer :: n, i, j, k
-        double precision, dimension (:), allocatable :: calcPageRank
+        real :: x, m
+        integer :: n, i, j, counter
+        double precision, dimension (:), allocatable :: calcPageRank, sm
+        double precision :: before, precision
+
+        !m padrao google
+        m = 0.15
+
+        !setando before como 1 para entrar no looping e setando precision
+        before = 1
+        precision = 0.0005
+        counter = 0
 
         !obtendo n da matriz n x n, e alocando no vetor de pesos
         x = size(A)
         n = sqrt(x)
         allocate(calcPageRank(n))
+        allocate(sm(n))
 
-        !criando vetor de pesos
+        !multiplicando a matriz A pelo (1-m)
         do i = 1, n
-            calcPageRank(i) = 1.0/n
-        enddo
-
-        do i = 1, 100
             do j = 1, n
-                temp = 0
-                do k = 1, n
-                    temp = temp + A(k,j) * calcPageRank(k)
-                enddo
-                calcPageRank(j) = temp
+                A(i,j) = A(i,j) * (1-m)
             enddo
         enddo
 
-        !print *, n
+        !criando os vetores de pesos
+        do i = 1, n
+            calcPageRank(i) = 1.0/n
+            sm(i) = (1.0/n) * m
+        enddo
+
+        !formula iterativa pra calcular pesos ate a precisao desejada
+        do while(abs(before - calcPageRank(1)) > precision )
+            !pegando o primeiro valor antigo do vetor pesos para calcular a precisao pos loop
+            before = calcPageRank(1)
+
+
+            !multiplicando matriz (1-m)Ay
+            calcPageRank = MATMUL(A, calcPageRank)
+
+            !somando (1-m)Ay + sm
+            do j = 1, n
+                calcPageRank(j) = calcPageRank(j) + sm(j)
+            enddo
+            !counter para contar numero de iteracoes
+            counter = counter + 1
+        enddo
+
+        !print*, "total de iteracoes na formula: ", counter
 
     end function
 
